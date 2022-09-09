5410dbc4fd69f5cad44c46ac85ef4ff9173f65f2
Felipe Costa Gomes <73756949+FelipeCosmes@users.noreply.github.com>
2022-09-09 11:37:48 -0300

    Atualizando o código cpp e o CMakelists

--- fastFaceLandmarkDetection.cpp
+++ fastFaceLandmarkDetection.cpp
@@ -10,7 +10,7 @@ using namespace std;
 using namespace dlib;
 
 //Desenhar linha
-void drawPolyline(cv::Mat &image, full_object_detection landmarks, int start, int end, bool isClosed=false){
+void desenhalinha(cv::Mat &image, full_object_detection landmarks, int start, int end, bool isClosed=false){
     std::vector<cv::Point> points;
     for(int i=start; i<=end; i++){
         points.push_back(cv::Point(landmarks.part(i).x(), landmarks.part(i).y()));
@@ -18,68 +18,60 @@ void drawPolyline(cv::Mat &image, full_object_detection landmarks, int start, in
     cv::polylines(image, points, isClosed, cv::Scalar(0, 255, 255), 2, 16);
 }
 
-void drawPolylines(cv::Mat &image, full_object_detection landmarks){
-/*    drawPolyline(image, landmarks, 0, 16);              //jaw line
-    drawPolyline(image, landmarks, 17, 21);             //left eyebrow
-    drawPolyline(image, landmarks, 22, 26);             //right eyebrow
-    drawPolyline(image, landmarks, 27, 30);             //Nose bridge
-    drawPolyline(image, landmarks, 30, 35, true);       //lower nose*/
-    drawPolyline(image, landmarks, 36, 41, true);       //left eye
-    drawPolyline(image, landmarks, 42, 47, true);       //right eye
-  //  drawPolyline(image, landmarks, 48, 59, true);       //outer lip
-//    drawPolyline(image, landmarks, 60, 67, true);       //inner lip
+void desenhalinhas(cv::Mat &image, full_object_detection landmarks){
+    desenhalinha(image, landmarks, 36, 41, true);       //olho esquerdo
+    desenhalinha(image, landmarks, 42, 47, true);       //olho direito
 }
 
-//finds face landmark points and draw polylines around face landmarks
-void findFaceLandmarksAndDrawPolylines(Mat &frame, frontal_face_detector faceDetector, shape_predictor landmarkDetector, 
+//encontra os pontos da face e desenha as linhas sobre os pontos dos olhos
+void encontraLandmarks(Mat &frame, frontal_face_detector faceDetector, shape_predictor landmarkDetector,
     std::vector<dlib::rectangle> &faces, float resizeScale, int skipFrames, int frameCounter){
 
-        //to store resized image
+        //armazenar imagem redimensionada
         Mat smallFrame;
 
-        //resize frame to smaller image
+        //redimensiona frame para imagem menor
         resize(frame, smallFrame, Size(), 1.0/resizeScale, 1.0/resizeScale);
 
-        //change to dlib image format
+        //muda para o formato da dlib
         cv_image<bgr_pixel> dlibImageSmall(smallFrame);
         cv_image<bgr_pixel> dlibImage(frame);
 
-        //detect faces at interval of skipFrames
+        //detecta faces no intervalo de quadros
         if(frameCounter % skipFrames == 0){
             faces = faceDetector(dlibImageSmall);
         }
 
-        //loop over faces
+        //loop sobre rostos
         for(int i=0; i<faces.size(); i++){
-            
-            //scale the rectangle coordinates as we did face detection on resized smaller image
+
+            //dimensione as coordenadas do retângulo como fizemos a detecção de rosto em uma imagem menor redimensionada
             dlib::rectangle rect(int(faces[i].left() * resizeScale),
                 int(faces[i].top() * resizeScale),
                 int(faces[i].right() * resizeScale),
                 int(faces[i].bottom() * resizeScale));
 
-            //Face landmark detection
+            //detecção dos landmarks faciais
             full_object_detection faceLandmark = landmarkDetector(dlibImage, rect);
 
-            //draw poly lines around face landmarks
-            drawPolylines(frame, faceLandmark);
+            //desenha as linhas sobre os landmarks
+            desenhalinhas(frame, faceLandmark);
 
-	    double P37_41_x = faceLandmark.part(37).x() - faceLandmark.part(41).x();
+            //cálculo das distancia euclidianas
+	        double P37_41_x = faceLandmark.part(37).x() - faceLandmark.part(41).x();
             double P37_41_y=  faceLandmark.part(37).y() - faceLandmark.part(41).y() ;
             double p37_41_sqrt=sqrt((P37_41_x * P37_41_x) + (P37_41_y * P37_41_y));
 
-            //
             double P38_40_x = faceLandmark.part(38).x() - faceLandmark.part(40).x();
             double P38_40_y = faceLandmark.part(38).y() - faceLandmark.part(40).y();
             double p38_40_sqrt=sqrt((P38_40_x * P38_40_x) + (P38_40_y * P38_40_y));
 
-            //
-            double P36_39_x = faceLandmark.part(36).x() - faceLandmark.part(39).x();  
+            double P36_39_x = faceLandmark.part(36).x() - faceLandmark.part(39).x();
             double P36_39_y = faceLandmark.part(36).y() - faceLandmark.part(39).y();
             double p36_39_sqrt=sqrt((P36_39_x * P36_39_x) + (P36_39_y * P36_39_y));
 
             //Calculo do EAR
-            double EAR= (p37_41_sqrt +  p38_40_sqrt)/(2* p36_39_sqrt);
+            double EAR = (p37_41_sqrt +  p38_40_sqrt)/(2* p36_39_sqrt);
 
             cout << "EAR value =  " << EAR << endl;
         }
@@ -87,49 +79,48 @@ void findFaceLandmarksAndDrawPolylines(Mat &frame, frontal_face_detector faceDet
 
 int main(){
 
-    //create videoCapture object to show wbecam video
+    //crie um objeto de captura de vídeo para mostrar o vídeo da webcam
     VideoCapture videoCapture(-1);
 
-    //Check if opencv is able to open camera
+    //checar se o opencv conseguiu abrir a camera
     if(!videoCapture.isOpened()){
         cout<<"can not open webcam"<<endl;
         return 0;
     }
 
-    //define the face detector
+    //definir os landmarks do rosto
     frontal_face_detector faceDetector = get_frontal_face_detector();
 
     //define landmark detector
     shape_predictor landmarkDetector;
-    
-    //load face landmark model
+
+    //carregar o modelo treinado de landmarks
     deserialize("../dlibAndModel/shape_predictor_68_face_landmarks.dat") >> landmarkDetector;
 
-    //define resize height
+    //redimensionar altura
     float resizeHeight = 480;
 
-    //define skip frames
+    //definir skip frames
     int skipFrames = 3;
 
-    //Get first frame
+    //Obter primeiro frame
     Mat frame;
     videoCapture >> frame;
 
-    //calculate resize scale
+    //redimensiona a escala
     float height = frame.rows;
     float resizeScale = height/resizeHeight;
 
-    //initiate the tickCounter
+    //inicia o tickCounter
     double tick = getTickCount();
     int frameCounter=0;
 
-    //create window to display frame
+    //cria uma tela para mostrar os frames
     namedWindow("frame", WINDOW_NORMAL);
 
-    //create variable to store fps
+    //criar variavel que armazena o fps
     double fps = 30.0;
 
-    //define to hold detected faces
     std::vector<dlib::rectangle> faces;
 
     while (1){
@@ -137,31 +128,27 @@ int main(){
             tick = getTickCount();
         }
 
-        //read a frame
+        //lê o frame
         videoCapture >> frame;
 
-        findFaceLandmarksAndDrawPolylines(frame, faceDetector, landmarkDetector, faces, resizeScale, skipFrames, frameCounter);
+        encontraLandmarks(frame, faceDetector, landmarkDetector, faces, resizeScale, skipFrames, frameCounter);
 
-        //change fps to string
         String fpss;
         fpss = to_string(fps);
 
-        //draw fps on the frame
-       // putText(frame, fpss, Point(50, 50), FONT_HERSHEY_SIMPLEX, 1, Scalar(0, 0, 255), 5);
-
-        //show frame
+        //mostra o frame
         imshow("frame", frame);
 
-        //Press esc to exit the program
+        //precionar o esc para sair da aplicação
         char key = waitKey(1);
         if(key == 27){
             break;
         }
 
-        //increment frame counter
+        //incrementa o frame counter
         frameCounter++;
 
-        //calculate fps after every 100 frames
+        //calcula fps depois de todo 100 frames
         if(frameCounter == 100){
             tick = ((double)getTickCount() - tick)/getTickFrequency();
             fps = 100.0/tick;
@@ -169,11 +156,11 @@ int main(){
         }
     }
 
-    //release video capture object
+    //release captura de video
     videoCapture.release();
 
-    //close all the opened windows
+    //fecha todas as janelas abertas
     destroyAllWindows();
-    
+
     return 0;
 }
